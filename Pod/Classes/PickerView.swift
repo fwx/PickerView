d82f00e064ee5d99886698520c04a8340d5db8a4
Unknown <lsv@feip.co>
2018-09-21 13:34:12 +1000

    CPU 100% fix

--- Pod/Classes/PickerView.swift
+++ Pod/Classes/PickerView.swift
@@ -436,8 +436,6 @@ open class PickerView: UIView {
         currentSelectedRow = row
         tableView.reloadData()
         
-        if self.numberOfRowsByDataSource == 0 { return }
-        
         repeat {
             // This line adjust the contentInset to UIEdgeInsetZero because when the PickerView are inside of a UIViewController 
             // presented by a UINavigation controller, the tableView contentInset is affected.
